634dee062f3591e24fb48070c63c6b5435bee88c
Bushty <bastian@gerkum.de>
2024-11-06 14:02:15 +0100

    basic structure

--- beverages.py
+++ beverages.py
@@ -26,3 +26,14 @@ class BeverageHistoryItem:
 class BeverageHistory:
     def __init__(self, beverage):
         pass
+
+def get_initialized_beverages():
+    beverages = []
+    beverages.append(Beverage("Bier 0,3l", 1.50, 0.00, 100.00, True))
+    beverages.append(Beverage("Bier 0,5l", 2.00, 0.00, 100.00, True))
+    beverages.append(Beverage("Shot", 1.00, 0.00, 100.00, True))
+    beverages.append(Beverage("Mische", 3.00, 0.00, 100.00, True))
+    beverages.append(Beverage("Cider", 2.00, 0.00, 100.00, True))
+    beverages.append(Beverage("Bachwasser", 2.00, 0.00, 100.00, True))
+
+    return beverages
